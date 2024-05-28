d60a226b8809c346dc3ba71af68bfb8cdf55b20f
Mukhlisa8717 <mukhlisa.saidaminova08@gmail.com>
2024-05-28 11:21:11 +0500

    cart page changed

--- src/components/empty/Empty.jsx
+++ src/components/empty/Empty.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import emptyImg from "../../assets/emptyImg.png"
-import './Empty.scss'
+import React from "react";
+import emptyImg from "../../assets/emptyImg.png";
+import "./Empty.scss";
 
 const Empty = () => {
   return (
-    <section className='empty'>
-        <div className="container empty__img">
-            <img src={emptyImg} alt="empty" />
-        </div>
-    </section>
-  )
-}
+    <div className="empty">
+      <img src={emptyImg} alt="empty" />
+    </div>
+  );
+};
 
-export default Empty
+export default Empty;
